5ebeedda55169414cc3b7e52b18a588adf0b3471
Isaac HesselRobinson <isaac.hesselrobinson@mymail.champlain.edu>
2023-10-14 23:31:07 -0400

    --Catcher more efficient about finishing off blocked cat

--- CatchTheCat/Assets/Scripts/Catcher.cs
+++ CatchTheCat/Assets/Scripts/Catcher.cs
@@ -7,16 +7,27 @@ public class Catcher : MonoBehaviour
     [SerializeField] GameManager gameManager;
     public Node TakeTurn(List<Node> path)
     {
-        if(path.Count == 0)
+        Node position;
+        //If the cat is at the edge, just block somewhere random
+        if (path.Count == 0)
         {
             int randPos = Random.Range(0, gameManager.GetNumNodes());
             while(gameManager.GetNodeAt(randPos).GetBlocked() || gameManager.GetNodeAt(randPos).GetIsCat())
             {
                 randPos = Random.Range(0, gameManager.GetNumNodes());
             }
-            path.Add(gameManager.GetNodeAt(randPos));
+            position = gameManager.GetNodeAt(randPos);
         }
-        Node position = path.First();
+        //Normally, block the edge the cat aims for
+        else if(path.First().GetIsEdge())
+        {
+            position = path.First();
+        }
+        //if you've got the cat trapped, move in for the kill
+        else
+        {
+            position = path.Last();
+        }
         //moves on the board
         position.Block();
         return position;
