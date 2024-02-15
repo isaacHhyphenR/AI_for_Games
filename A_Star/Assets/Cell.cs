ba7a489ea28fe9efe9fab8d81c4f378d7502c2f7
Isaac HesselRobinson <isaac.hesselrobinson@mymail.champlain.edu>
2024-02-14 19:14:07 -0500

    --First draft A*, cells have accurate neighbours

--- A_Star/Assets/Cell.cs
+++ A_Star/Assets/Cell.cs
@@ -1,11 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
     [SerializeField] Renderer render;
     [SerializeField] LineRenderer line;
-    [HideInInspector] public Vector2 sector;
-    public Transform linePos;
+    [HideInInspector] public Sector sector;
+    [HideInInspector] public Vector3 position;
+
+    public List<Cell> neighbours = new List<Cell>();
+    //A* data
+    public float heuristic = -1;
+    public float costSoFar = 0;
+    public float expectedCost = 0;
+    public Cell parent = null;
+    private void Start()
+    {
+        position = line.transform.position;
+    }
+    public void ResetPathfinding()
+    {
+        heuristic = -1;
+        costSoFar = 0;
+        expectedCost = 0;
+        parent = null;
+    }
+
+    public void CalculateHeuristic(Vector3 destination)
+    {
+        heuristic = Mathf.Abs((destination - position).magnitude);
+    }
+    public float GetHeuristic()
+    {
+        return heuristic;
+    }
+    public float CostToMove(Vector3 origin)
+    {
+        return Mathf.Abs((origin - position).magnitude);
+    }
 
     public float GetSize()
     {
@@ -20,19 +52,37 @@ public class Cell : MonoBehaviour
     {
         render.material.color = color;
     }
+
+    public void AddNeighbour(Cell neighbour)
+    {
+        neighbours.Add(neighbour);
+    }
+
     //Draws a line from here to destination
     public void DrawLine(Vector3 destination)
     {
         line.enabled = true;
-        line.SetPositions(new Vector3[2] {linePos.position, destination });
+        line.SetPositions(new Vector3[2] { position, destination });
+        sector.SetHasLines(true);
     }
     public void ClearLine()
     {
         line.enabled = false;
     }
 
+    //left click
     private void OnMouseDown()
     {
-        GridManager.instance.Pathfind(this);
+        GridManager.instance.SetOrigin(this);
+        GridManager.instance.Pathfind();
+    }
+    void OnMouseOver()
+    {
+        //right click
+        if(Input.GetMouseButtonDown(1))
+        {
+            GridManager.instance.SetDestination(this);
+            //GridManager.instance.Pathfind();
+        }
     }
 }
--- A_Star/Assets/GridManager.cs
+++ A_Star/Assets/GridManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-struct Sector
+public struct Sector
 {
     public Cell[,] cells;
     Color sectorColor;
@@ -28,7 +30,7 @@ struct Sector
         //The corner from which the cells begin generating
         Vector3 originPosition = sectorPosition;
         originPosition.x -= width * cellSize / 2;
-        originPosition.z -= width * cellSize / 2;
+        originPosition.z += width * cellSize / 2;
         //Creates all the cells
         for (int x = 0; x < width; x++)
         {
@@ -36,15 +38,30 @@ struct Sector
             {
                 GameObject newCellObj = GameObject.Instantiate(cellPrefab, originPosition, cellPrefab.transform.rotation);
                 Cell newCell = newCellObj.GetComponent<Cell>();
-                newCell.transform.position = new Vector3(newCell.transform.position.x + (x * cellSize), newCell.transform.position.y, newCell.transform.position.z + (y * cellSize));
+                newCell.transform.position = new Vector3(newCell.transform.position.x + (x * cellSize), newCell.transform.position.y, newCell.transform.position.z + (-y * cellSize));
                 newCell.SetColor(sectorColor);
                 newCell.name = (int)sectorID.x + "," + (int)sectorID.y + "_Cell_" + x + "," + y;
-                newCell.sector = sectorID;
-                cells[x,y] = newCell;
+                newCell.sector = this;
+                cells[x, y] = newCell;
             }
         }
     }
+
+    public void ClearLines()
+    {
+        hasLines = false;
+        foreach (Cell cell in cells)
+        {
+            cell.ClearLine();
+        }
+    }
+    public void SetHasLines(bool line)
+    {
+        hasLines = line; ;
+    }
 }
+
+
 public class GridManager : MonoBehaviour
 {
     [Tooltip("The prefab for each individual cell.")]
@@ -58,6 +75,9 @@ public class GridManager : MonoBehaviour
     Sector[,] sectors;
     float cellSize;
 
+    Cell origin;
+    Cell destination;
+
     public static GridManager instance;
 
     private void Start()
@@ -69,19 +89,145 @@ public class GridManager : MonoBehaviour
         //The corner from which the cells begin generating
         Vector3 originPosition = transform.position;
         originPosition.x -= (sectorActualWidth * gridWidth / 2) - sectorActualWidth / 2;
-        originPosition.z -= (sectorActualWidth * gridWidth / 2) - sectorActualWidth / 2;
+        originPosition.z += (sectorActualWidth * gridWidth / 2) - sectorActualWidth / 2;
         //Generates each sector
         for (int x = 0; x < gridWidth; x++)
         {
             for(int y = 0; y < gridWidth; y++)
             {
-                Vector3 pos = new Vector3(originPosition.x + x * sectorActualWidth, originPosition.y, originPosition.z + y * sectorActualWidth);
+                Vector3 pos = new Vector3(originPosition.x + x * sectorActualWidth, originPosition.y, originPosition.z + (-y * sectorActualWidth));
                 Sector newSector = new Sector(sectorWidth, cellPrefab, GenerateColor(lightness), pos, new Vector2(x,y));
                 sectors[x,y] = newSector;
             }
         }
+        GenerateCellNeighbours();
         //Instance
         GridManager.instance = this;
+    }
+
+    private void GenerateCellNeighbours()
+    {
+        int maxSector = gridWidth - 1;
+        int maxCell = sectorWidth - 1;
+        //Cycles through every sector
+        for( int i = 0; i < gridWidth; i++)
+        {
+            for(int j = 0; j < gridWidth; j++)
+            {
+                Sector sector = sectors[i, j];
+                //Cycles through every cell
+                for (int x = 0; x < sectorWidth; x++)
+                {
+                    for (int y = 0; y < sectorWidth; y++)
+                    {
+                        ///NORTH EAST
+                        if (x > 0 && y > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x - 1, y - 1]);
+                        }
+                        else if(x == 0 && y == 0 && i > 0 && j > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i - 1, j - 1].cells[maxCell, maxCell]);
+                        }
+                        else if (x == 0 && i > 0 && y > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i - 1, j].cells[maxCell, y - 1]);
+                        }
+                        else if (y == 0 && j > 0 && x > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j - 1].cells[x - 1, maxCell]);
+                        }
+                        ///NORTH
+                        if (y > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x, y - 1]);
+                        }
+                        else if (j > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j - 1].cells[x, maxCell]);
+                        }
+                        ///NORTH WEST
+                        if (x < maxCell && y > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x + 1, y - 1]);
+                        }
+                        else if (x == maxCell && y == 0 && i < maxSector && j > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i + 1, j - 1].cells[0, maxCell]);
+                        }
+                        else if (x == maxCell && i < maxSector && y > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i + 1, j].cells[0, y - 1]);
+                        }
+                        else if (y == 0 && j > 0 && x < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j - 1].cells[x + 1, maxCell]);
+                        }
+                        ///WEST
+                        if (x < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x + 1, y]);
+                        }
+                        else if(i < maxSector)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i + 1, j].cells[0, y]);
+                        }
+                        ///SOUTH WEST
+                        if (x < maxCell && y < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x + 1, y + 1]);
+                        }
+                        else if (x == maxCell && y == maxCell && i < maxSector && j < maxSector)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i + 1, j + 1].cells[0, 0]);
+                        }
+                        else if (x == maxCell && i < maxSector && y < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i + 1, j].cells[0, y + 1]);
+                        }
+                        else if (y == maxCell && j < maxSector && x < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j + 1].cells[x + 1, 0]);
+                        }
+                        ///SOUTH
+                        if (y < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x, y + 1]);
+                        }
+                        else if (j < maxSector)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j + 1].cells[x, 0]);
+                        }
+                        ///SOUTH EAST
+                        if (x > 0 && y < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x - 1, y + 1]);
+                        }
+                        else if (x == 0 && y == maxCell && i > 0 && j < maxSector)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i - 1, j + 1].cells[maxCell, 0]);
+                        }
+                        else if (x == 0 && i > 0 && y < maxCell)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i - 1, j].cells[maxCell, y + 1]);
+                        }
+                        else if (y == maxCell && j < maxSector && x > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i, j + 1].cells[x - 1, 0]);
+                        }
+                        ///EAST
+                        if (x > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sector.cells[x - 1, y]);
+                        }
+                        else if (i > 0)
+                        {
+                            sector.cells[x, y].AddNeighbour(sectors[i - 1, j].cells[maxCell, y]);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public static Color GenerateColor(float lightness)
@@ -102,24 +248,119 @@ public class GridManager : MonoBehaviour
     }
 
     //Draws a path
-    public void Pathfind(Cell origin)
+    public void Pathfind()
     {
-        //Clears all lines currently drawn on the board
-        foreach(Sector sector in sectors)
+        bool foundPath = false;
+        //Only Pathfinds if it has an origin & a destination
+        if(origin != null && destination != null)
         {
-            if (sector.hasLines)
+            //Clears all lines currently drawn on the board
+            foreach (Sector sector in sectors)
             {
-                foreach(Cell cell in sector.cells)
+                //if (sector.hasLines) //I want this for optimisation but it always results false. I think the Cell.sector variable not setting properly
                 {
-                    cell.ClearLine();
+                    sector.ClearLines();
                 }
             }
+
+            List<Cell> openList = new List<Cell>();
+            List<Cell> closedList = new List<Cell>();
+            openList.Add(origin);
+
+            while(openList.Count > 0 && !foundPath)
+            {
+                Cell head = openList.First();
+                //Sets the open list cell with the lowest expected cost as head
+                foreach (Cell cell in openList)
+                {
+                    if(cell.heuristic < 0)
+                    {
+                        cell.CalculateHeuristic(destination.position);
+                    }
+                    cell.expectedCost = cell.costSoFar + cell.heuristic;
+                    if( cell.expectedCost < head.expectedCost)
+                    {
+                        head = cell;
+                    }
+                }
+                //Removes the head from the open list & searches it's successors
+                openList.Remove(head);
+                //head.SetColor(new Color(0, 0, 0));
+                closedList.Add(head);
+                foreach(Cell cell in head.neighbours)
+                {
+                    //If it's the goal, congrats!
+                    if(cell == destination)
+                    {
+                        cell.parent = head;
+                        foundPath = true;
+                        break;
+                    }
+                    //If it's not on the closed list, move on to open list
+                    else if(!closedList.Contains(cell))
+                    {
+                        float newCellCost = head.costSoFar + cell.CostToMove(head.position);
+                        //If it's not on the open list, add it
+                        if (!openList.Contains(cell))
+                        {
+                            cell.parent = head;
+                            cell.costSoFar = newCellCost;
+                            cell.CalculateHeuristic(destination.position);
+                            cell.expectedCost = cell.heuristic + cell.costSoFar;
+                            openList.Add(cell);
+                        }
+                        //If it's on the open list but this is a better path, overwrite the old path
+                        else if(newCellCost < cell.costSoFar)
+                        {
+                            cell.parent = head;
+                            cell.costSoFar = newCellCost;
+                            cell.CalculateHeuristic(destination.position);
+                            cell.expectedCost = cell.heuristic + cell.costSoFar;
+                        }
+                    }
+                }
+            }
+            //Retraces its steps to draw the path
+            if(foundPath)
+            {
+                destination.DrawLine(origin.position);
+            }
+
+            /*
+        iii) if a node with the same position as
+            successor is in the OPEN list which has a
+           lower f than successor, skip this successor
+        iV) if a node with the same position as
+            successor is in the CLOSED list which has
+            a lower f than successor, skip this successor
+            otherwise, add  the node to the open list
+     end(for loop)
+
+                e) push q on the closed list
+    end(while loop)
+            */
+
+
+            //Pathfinds
+            //origin.DrawLine(destination.position);
+                /*
+                foreach (Cell cell in sectors[(int)origin.sector.x, (int)origin.sector.y].cells)
+                {
+                    cell.DrawLine(origin.position);
+                    sectors[(int)cell.sector.x, (int)cell.sector.y].hasLines = true;
+                }
+                */
         }
-        //Pathfinds
-        foreach(Cell cell in sectors[(int)origin.sector.x, (int)origin.sector.y].cells)
-        {
-            cell.DrawLine(origin.linePos.position);
-            sectors[(int)cell.sector.x, (int)cell.sector.y].hasLines = true;
-        }
+    }
+
+    public void SetOrigin(Cell newOrigin)
+    {
+        origin = newOrigin;
+        Pathfind();
+    }
+    public void SetDestination(Cell newDestination)
+    {
+        destination = newDestination;
+        Pathfind();
     }
 }
